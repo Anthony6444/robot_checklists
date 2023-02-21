fa4f32ada0999df432c1841f03772a8d328ddba1
Anthony <anthonydc1972@outlook.com>
2023-02-20 20:44:06 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# robot_checklists
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/robot_checklists/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.robot_checklists
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
