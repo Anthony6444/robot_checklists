a7ee279cf5e0849b56b6b8a46169d796e6227c80
Anthony <anthonydc1972@outlook.com>
2023-02-20 22:34:10 -0600

    basic outline

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:robot_checklists/pageladder/home.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,120 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green)),
+        home: HomePage(),
       ),
     );
+  }
+}
+
+class AppState extends ChangeNotifier {}
+
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = PageLadderHome();
+        break;
+      default:
+        page = const Placeholder();
+    }
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        bottomNavigationBar: NavigationBar(
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.home),
+                label: "Home",
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.person_add),
+                label: "Scouting",
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.gamepad),
+                label: "Drivers",
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.account_tree),
+                label: "Network",
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.add_chart),
+                label: "Stats",
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.grid_on),
+                label: "Field",
+              )
+            ]),
+        body: Row(children: [
+          SafeArea(
+              child: NavigationRail(
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text("Home"),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.person_add),
+                label: Text("Scouting"),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.gamepad),
+                label: Text("Drivers"),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.account_tree),
+                label: Text("Network"),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.add_chart),
+                label: Text("Stats"),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.grid_on),
+                label: Text("Field"),
+              ),
+            ],
+          )),
+          Expanded(
+            child: SafeArea(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ),
+        ]),
+      );
+    });
   }
 }
---
+++ lib/pageladder/scouting.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageLadderScouting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Scouting"),
+    );
+  }
+}
