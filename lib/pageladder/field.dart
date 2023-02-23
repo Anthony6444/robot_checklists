09cc00349f3e64e52f49bd7571d90651fad08038
Anthony <anthonydc1972@outlook.com>
2023-02-23 12:34:16 -0600

    add field diagram

--- lib/pageladder/field.dart
+++ lib/pageladder/field.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:robot_checklists/main.dart';
+import 'package:vector_math/vector_math.dart';
 
 class PageLadderField extends StatelessWidget {
+  const PageLadderField({super.key});
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<AppState>();
-    return Center(
-      child: Text("Field"),
+    return Transform.rotate(
+      angle: radians(MediaQuery.of(context).size.aspectRatio > 1 ? 90 : 0),
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Image.asset("assets/field_diagram_cropped.png");
+      }),
     );
   }
 }
