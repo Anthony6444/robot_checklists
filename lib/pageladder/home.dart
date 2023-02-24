8b46276ebeb7bf5c1f9d04d511f1176e3b0001cf
Anthony <anthonydc1972@outlook.com>
2023-02-23 20:07:36 -0600

    add hompage

--- lib/pageladder/home.dart
+++ lib/pageladder/home.dart
@@ -3,16 +3,37 @@ import 'package:provider/provider.dart';
 import 'package:robot_checklists/main.dart';
 
 class PageLadderHome extends StatelessWidget {
+  TextStyle style = TextStyle(fontFamily: "BraveEightyOne", fontSize: 40);
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<AppState>();
-    return const Center(
-      child: Card(
-        child: Padding(
-          padding: EdgeInsetsDirectional.all(10),
-          child: Text("Fancy Homepage"),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Card(
+          child: Padding(
+            padding: const EdgeInsetsDirectional.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Trinity".toUpperCase(),
+                  style: style,
+                ),
+                Image.asset(
+                  "assets/logo.png",
+                  scale: 0.2,
+                ),
+                Text(
+                  "Robotics".toUpperCase(),
+                  style: style,
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
